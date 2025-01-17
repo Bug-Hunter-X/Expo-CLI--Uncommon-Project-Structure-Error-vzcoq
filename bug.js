c041a6fbec84a2e78eb783305b135586f89b58a7
BugHunter-X <askzenai@gmail.com>
2025-01-17 06:56:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI with a project that has a complex or unusual project structure.  It is often manifested as an error message stating that Expo cannot find or build certain dependencies. The specific error message might vary depending on the issue.
